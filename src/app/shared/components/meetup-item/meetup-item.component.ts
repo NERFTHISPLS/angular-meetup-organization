516d9d1e8b90c56921926102134559f4b73955f5
NERFTHIS <mustaevs@gmail.com>
2024-03-31 15:06:22 +0300

    Create meetup item component

---
+++ src/app/shared/components/meetup-item/meetup-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meetup-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './meetup-item.component.html',
+  styleUrl: './meetup-item.component.scss'
+})
+export class MeetupItemComponent {
+
+}
