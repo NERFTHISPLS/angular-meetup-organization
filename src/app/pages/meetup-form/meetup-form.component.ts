675bd5f1712b799466f192f1c33004c50cdda8de
NERFTHIS <mustaevs@gmail.com>
2024-03-31 20:18:44 +0300

    Create the meetup-form

---
+++ src/app/pages/meetup-form/meetup-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meetup-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './meetup-form.component.html',
+  styleUrl: './meetup-form.component.scss'
+})
+export class MeetupFormComponent {
+
+}
