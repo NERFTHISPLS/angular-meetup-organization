85e14400804ae0f6e9a3ada5d18d3ee25178bd1c
NERFTHIS <mustaevs@gmail.com>
2024-03-31 14:50:19 +0300

    Create the all meetups page

---
+++ src/app/pages/all-meetups/all-meetups.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-meetups',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-meetups.component.html',
+  styleUrl: './all-meetups.component.scss'
+})
+export class AllMeetupsComponent {
+
+}
