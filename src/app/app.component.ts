6011a724bdb6b8f6de6f30603ad06e401ba775ce
NERFTHIS <mustaevs@gmail.com>
2024-03-31 15:09:33 +0300

    Create markup for the meetup list item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './shared/components/header/header.component';
+import { AllMeetupsComponent } from './pages/all-meetups/all-meetups.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AllMeetupsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-meetup-organization';
--- src/app/pages/all-meetups/all-meetups.component.ts
+++ src/app/pages/all-meetups/all-meetups.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
+import { MeetupItemComponent } from '../../shared/components/meetup-item/meetup-item.component';
+
 @Component({
   selector: 'app-all-meetups',
   standalone: true,
-  imports: [],
+  imports: [MeetupItemComponent],
   templateUrl: './all-meetups.component.html',
-  styleUrl: './all-meetups.component.scss'
+  styleUrl: './all-meetups.component.scss',
 })
-export class AllMeetupsComponent {
-
-}
+export class AllMeetupsComponent {}
