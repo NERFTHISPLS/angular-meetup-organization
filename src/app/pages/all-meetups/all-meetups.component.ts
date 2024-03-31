54edbe47683fb189c8c6765f6d53df9224ccc29e
NERFTHIS <mustaevs@gmail.com>
2024-03-31 17:24:45 +0300

    Style the new-meetup-btn

--- src/app/pages/all-meetups/all-meetups.component.ts
+++ src/app/pages/all-meetups/all-meetups.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { MeetupItemComponent } from '../../shared/components/meetup-item/meetup-item.component';
+import { NewMeetupBtnComponent } from '../../shared/components/new-meetup-btn/new-meetup-btn.component';
 
 @Component({
   selector: 'app-all-meetups',
   standalone: true,
-  imports: [MeetupItemComponent],
+  imports: [MeetupItemComponent, NewMeetupBtnComponent],
   templateUrl: './all-meetups.component.html',
   styleUrl: './all-meetups.component.scss',
 })
