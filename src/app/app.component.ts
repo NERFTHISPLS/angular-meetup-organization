04e488c81049f0e9f8630303b5459bb0ac16b62b
NERFTHIS <mustaevs@gmail.com>
2024-03-31 20:38:10 +0300

    Create form markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './shared/components/header/header.component';
 import { AllMeetupsComponent } from './pages/all-meetups/all-meetups.component';
+import { MeetupFormComponent } from './pages/meetup-form/meetup-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AllMeetupsComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AllMeetupsComponent,
+    MeetupFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
