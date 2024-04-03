d5c4ab67a150a4c3dd9f2c46d462241e865dd8e0
NERFTHIS <mustaevs@gmail.com>
2024-04-03 18:38:06 +0300

    Get data from registration form and add validation

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  styleUrl: './registration.component.scss',
 })
 export class RegistrationComponent {
+  private formBuilder = inject(FormBuilder);
 
+  public registrationForm = this.formBuilder.nonNullable.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  public firstNameControl = this.registrationForm.controls.firstName;
+  public lastNameControl = this.registrationForm.controls.lastName;
+  public emailControl = this.registrationForm.controls.email;
+  public passwordControl = this.registrationForm.controls.password;
+
+  public isFormSubmitted = false;
+
+  public submitRegistation() {
+    if (this.registrationForm.invalid) {
+      this.registrationForm.markAllAsTouched();
+
+      return;
+    }
+
+    console.log(this.registrationForm.controls);
+  }
 }
