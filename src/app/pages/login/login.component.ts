12c397b3bb348acd61f20544941035f3c7346473
NERFTHIS <mustaevs@gmail.com>
2024-04-03 20:43:29 +0300

    Get data from the login form and add validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
 import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnDestroy {
   private userService = inject(UserService);
+  private formBuilder = inject(FormBuilder);
 
   public wasJustRegistered = this.userService.wasJustRegistered;
+
+  public loginForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  public emailControl = this.loginForm.controls.email;
+  public passwordControl = this.loginForm.controls.password;
+
+  public submitLogin() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
 
   ngOnDestroy(): void {
     this.userService.wasJustRegistered = false;
