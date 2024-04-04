482a8893fe74a49220f9d193bc0a160960d20fe9
NERFTHIS <mustaevs@gmail.com>
2024-04-04 18:37:09 +0300

    Sort meetups by date

--- src/app/shared/services/meetup.service.ts
+++ src/app/shared/services/meetup.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, reduce, tap } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 
@@ -19,8 +19,26 @@ export class MeetupService {
     const urlToFetch = `${apiUrl}/meetup`;
 
     return this.httpClient.get<Meetup[]>(urlToFetch).pipe(
+      map((response: Meetup[]) => {
+        const meetupsNotHeld = response.filter(
+          (item) => Date.now() <= new Date(item.time).getTime()
+        );
+        const meetupsHeld = response.filter(
+          (item) => Date.now() > new Date(item.time).getTime()
+        );
+
+        meetupsNotHeld.sort(
+          (a, b) => new Date(a.time).getTime() - new Date(b.time).getTime()
+        );
+        meetupsHeld.sort(
+          (a, b) => new Date(b.time).getTime() - new Date(a.time).getTime()
+        );
+
+        return [...meetupsNotHeld, ...meetupsHeld];
+      }),
       tap((response: Meetup[]) => {
         this._allMeetups = response;
+        console.log(response);
       })
     );
   }
