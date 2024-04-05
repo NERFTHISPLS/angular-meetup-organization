fd1932c65d9cc51d9d29aa65f658af7c68e97d48
NERFTHIS <mustaevs@gmail.com>
2024-04-05 12:04:21 +0300

    Implement go back btn

--- src/app/pages/meetup-form/meetup-form.component.ts
+++ src/app/pages/meetup-form/meetup-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-meetup-form',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './meetup-form.component.html',
   styleUrl: './meetup-form.component.scss',
 })
-export class MeetupFormComponent {}
+export class MeetupFormComponent {
+  private location = inject(Location);
+
+  public routerReturnBack() {
+    this.location.back();
+  }
+}
