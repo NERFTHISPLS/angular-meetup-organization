12ca1979f95d5da01dc656d6a58f0a2d141184fb
NERFTHIS <mustaevs@gmail.com>
2024-04-06 20:30:14 +0300

    Create search component

---
+++ src/app/shared/components/meetup-search/meetup-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meetup-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './meetup-search.component.html',
+  styleUrl: './meetup-search.component.scss'
+})
+export class MeetupSearchComponent {
+
+}
