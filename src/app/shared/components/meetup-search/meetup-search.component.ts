a904fa2cc1f932bbd06d672a4449af87e9f83a95
NERFTHIS <mustaevs@gmail.com>
2024-04-06 22:23:24 +0300

    Implement search

--- src/app/shared/components/meetup-search/meetup-search.component.ts
+++ src/app/shared/components/meetup-search/meetup-search.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MeetupService } from '../../services/meetup.service';
 
 @Component({
   selector: 'app-meetup-search',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './meetup-search.component.html',
-  styleUrl: './meetup-search.component.scss'
+  styleUrl: './meetup-search.component.scss',
 })
 export class MeetupSearchComponent {
+  public meetupService = inject(MeetupService);
+  private formBuilder = inject(FormBuilder);
 
+  public searchForm = this.formBuilder.nonNullable.group({
+    query: [''],
+    from: [''],
+    to: [''],
+  });
+
+  public search() {
+    const search = this.searchForm.controls.query.value;
+    const from = this.searchForm.controls.from.value;
+    const to = this.searchForm.controls.to.value;
+
+    const fromDate = from ? new Date(from) : undefined;
+    const toDate = to ? new Date(to) : undefined;
+
+    this.meetupService.setMeetupsBy({
+      query: search,
+      from: fromDate,
+      to: toDate,
+    });
+  }
+
+  public reset() {
+    this.searchForm.reset();
+    this.meetupService.allMeetups = this.meetupService.allMeetupsOriginal;
+  }
 }
