3617aeace43efcd6af10d9cb7373ad9083be143c
NERFTHIS <mustaevs@gmail.com>
2024-04-06 23:37:42 +0300

    Add links at the about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../shared/services/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
